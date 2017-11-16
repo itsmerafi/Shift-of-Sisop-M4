9f5187ca37c710076896e2c877c21e956be8deae
Ayas Faikar Nafis <ayasfn@gmail.com>
2017-11-16 19:11:26 +0700

    selesai

--- Soal2.c
+++ Soal2.c
@@ -52,3 +52,61 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,off_t
         closedir(dp);
         return 0;
 }
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,struct fuse_file_info *fi)
+{
+    (void) fi;
+    int fd1308;
+    char filepath[500];
+    sprintf(filepath,"%s%s",dirpath,path);
+    int res1308;
+              
+    char ekstensii[500];
+    int i=0;
+    while(i < strlen(filepath) && filepath[i] != '.'){
+        i++;
+    }
+    strcpy(ekstensii, filepath+i);
+
+    char iniperintah[500];
+    fd1308 = open(filepath, O_RDONLY);
+    if (fd1308 == -1)
+        return -errno;
+    else{
+        if(strcmp(ekstensii, ".pdf") == 0 || strcmp(ekstensii, ".doc") == 0 || strcmp(ekstensii, ".txt") == 0){
+            system("zenity --width 450 --error --title 'Error' --text 'Terjadi Kesalahan! File berisi konten berbahaya.'");
+            char folderBaru1323[500];
+            
+            //ambil direktori
+            sprintf(folderBaru1323,"%s/rahasia",dirpath);
+            
+             //direktori secret ?
+            struct stat s;
+            if (stat(folderBaru1323, &s) != 0)          //ga ada secret
+                mkdir(folderBaru1323, 0777); //buat secret
+                    
+            sprintf(iniperintah, "mv %s %s/%s.ditandai && chmod 000 %s/%s.ditandai", filepath, folderBaru1323,path,folderBaru1323,path); //rename+move+chmod
+            system(iniperintah);
+            return -errno;                                   
+        }
+              
+        res1308 = pread(fd1308, buf, size, offset);
+        if (res1308 == -1)
+            res1308 = -errno;
+            close(fd1308);
+        }
+        return res1308;
+}
+
+
+static struct fuse_operations xmp_oper = {
+  .getattr  = xmp_getattr,
+  .readdir  = xmp_readdir,
+  .read   = xmp_read,
+};
+
+int main(int argc, char *argv[])
+{
+  umask(0);
+  return fuse_main(argc, argv, &xmp_oper, NULL);
+}
